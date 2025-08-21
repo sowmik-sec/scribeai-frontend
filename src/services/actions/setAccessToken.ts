2ac114e49e5e8aa20be143050204c6d35b8f4996
sowmik-sec <sowmik.sec@gmail.com>
2025-08-21 21:11:51 +0600

    refactor: simplify redirect logic in setAccessToken function

--- src/services/actions/setAccessToken.ts
+++ src/services/actions/setAccessToken.ts
@@ -9,10 +9,7 @@ import { redirect } from "next/navigation";
 const setAccessToken = async (token: string, option?: any) => {
   const cookieStore = await cookies();
   cookieStore.set(authKey, token);
-  if (option && option.passwordChangeRequired) {
-    redirect("/dashboard/change-password");
-  }
-  if (option && !option.passwordChangeRequired && option.redirect) {
+  if (option && option.redirect) {
     redirect(option.redirect);
   }
 };
